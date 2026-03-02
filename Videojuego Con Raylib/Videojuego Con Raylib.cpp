163252526ef55a7b8edd77ea57293cea9a5f89cd
Guillermo Ventura <luisguillermoventurapiriz360@gmail.com>
2026-03-01 23:32:03 -0600

    Commit Agrege Colisiones entre Player y Block del centro (no contra parades), tambien limites de la ventana

--- Videojuego Con Raylib/Videojuego Con Raylib.cpp
+++ Videojuego Con Raylib/Videojuego Con Raylib.cpp
@@ -2,81 +2,120 @@
 #include <iostream>
 #include <raylib.h>
 
-
 using namespace std;
+
+int blockSize = 30;
+int maze[50][75];
+Rectangle block = Rectangle{500,500,80,80}; // .x, .y, .width, .height
 
 //Window default
 Color backgroundColor = { 20,16,75,255 };
-int windowWidth = 900;
-int windowHeight = 1000;
+int windowWidth = 950;
+int windowHeight = 1500;
 
 //Player
-class Player{
+class Player {
 private:
     Vector2 position;
     Vector2 size;
     float speed;
 public:
-    Player(Vector2, Vector2,float);//Constructor
-    void movimiento();
-    int dibujar();
+    Player(Vector2 position, Vector2 size, float speed) {
+        this->position = position;
+        this->size = size;
+        this->speed = speed;//Constructor
+    }
+    void movimiento() {
+        float deltaTime = GetFrameTime();
+
+        if (IsKeyDown(KEY_RIGHT)) position.x += speed * deltaTime;
+        if (IsKeyDown(KEY_LEFT))  position.x -= speed * deltaTime;
+        if (IsKeyDown(KEY_UP))    position.y -= speed * deltaTime;
+        if (IsKeyDown(KEY_DOWN))  position.y += speed * deltaTime;
+
+        //Limite Window
+        windowWidth = GetScreenWidth();
+        windowHeight = GetScreenHeight();
+
+        if ((position.x - size.x) < 0) position.x = 0 + size.x;
+        if (position.x + size.x > windowWidth) position.x = windowWidth - size.x;
+        if (position.y - size.x < 0) position.y = 0 + size.x;
+        if (position.y + size.x > windowHeight) position.y = windowHeight - size.x;
+    }
+    int dibujarP1() {
+        DrawRectangleV(position, size, GRAY);
+        return 0;
+    }
+    Rectangle GetRect() {
+        return Rectangle{ position.x, position.y, size.x, size.y };
+    }
+    void DrawHitbox(bool isColliding) {
+        if (isColliding) {
+            DrawRectangleLinesEx(GetRect(), 3, RED);
+        }   
+    }
 };
 
-Player::Player(Vector2 _position, Vector2 _size, float _speed) {
-    position = _position;
-    size = _size;
-    speed = _speed;
-}
+void array2D() {
 
-void Player::movimiento() {
-    float deltaTime = GetFrameTime();
+    for (int y = 0; y < blockSize; y++) {
+        for (int x = 0; x < blockSize; x++) {
+            // Bordes
+            if (y == 0 || y == blockSize - 1 || x == 0 || x == blockSize - 1) {
+                maze[y][x] = 1;
+            }
+            // Paredes Aleatorias
 
-    if (IsKeyDown(KEY_RIGHT)) position.x += speed * deltaTime;
-    if (IsKeyDown(KEY_LEFT))  position.x -= speed * deltaTime;
-    if (IsKeyDown(KEY_UP))    position.y -= speed * deltaTime;
-    if (IsKeyDown(KEY_DOWN))  position.y += speed * deltaTime;
-}
-
-int Player::dibujar() {
-    DrawRectangleV(position, size, GRAY);
-    return 0;
-}
-
-//Block
-int mapa() {
-    const int blockSize = 40;
-    // Mapa del laberinto: # = Pared. = Camino
-    char map[10][10] = {
-        "##########""#........#""#.######.#""#.#....#.#""#.#.##.#.#""#.#.##...#""#.######.#""#........#""##########"
-    };
-    // Dibujar el laberinto
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            if (map[y][x] == '#') {
-                // Dibuja la pared (bloque)
-                DrawRectangle(x * blockSize, y * blockSize, blockSize, blockSize, DARKBLUE);
-                // Opcional: dibujar borde del bloque
-                DrawRectangleLines(x * blockSize, y * blockSize, blockSize, blockSize, BLACK);
+            else if (x % 4 == 0 && y % 4 == 0) {
+                maze[y][x] = 1;
             }
         }
     }
-    return 0;
 }
 
+void printArray2D() {
+    for (int y = 0; y < 50; y++) {
+        for (int x = 0; x < 50; x++) {
+            if (maze[y][x] == 1) {
+                // Posicion * tamano
+                DrawRectangle(x * blockSize, y * blockSize, blockSize, blockSize, RED);
+                // Bordes
+                DrawRectangleLines(x * blockSize, y * blockSize, blockSize, blockSize, BLUE);
+            }
+        }
+    }
+}
+
+/*
+int mapa() {
+    DrawRectangleLinesEx(block, 15,GREEN);
+    array2D();
+    printArray2D();
+    return 0;
+}
+*/
+
 int main(){
-    Player p1 = Player({ 400,225 }, { 40,40 }, 200.0f);
+
+    Player p1 = Player({ 400,225 }, { 20,20 }, 200.0f);
+
     InitWindow(windowHeight, windowWidth, "MazeRunner");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-
         ClearBackground(backgroundColor);
-        DrawText("Ventana Videojuego!", 200, 200, 30, LIGHTGRAY);
 
-
-        p1.dibujar();
+        bool isColliding = CheckCollisionRecs(p1.GetRect(), block);
         p1.movimiento();
+        DrawRectangleLinesEx(block, 15, GREEN);
+        p1.dibujarP1();
+        p1.DrawHitbox(isColliding);
+
+        array2D();
+        printArray2D();
+        
+        
 
         EndDrawing();
     }
